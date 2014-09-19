4f4cbf17094d3346898f8aa938de0226ca11c93b
Username <andreas@andreas-OptiPlex-755.(none)>
2014-09-19 10:29:44 +0200

    just testing commit

---
+++ sumitcode/TestingAdd/FoodiesLikeMeTwitter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package foodieslikemetwitter;
+
+import Gui.MainFrame;
+import java.awt.Dimension;
+
+/**
+ *
+ * @author sofkl_000
+ */
+public class FoodiesLikeMeTwitter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Dimension dimension = new Dimension(800,600);
+        MainFrame mainFrame = new MainFrame(dimension);
+    }
+    
+}
